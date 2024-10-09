d3322e387366ed67878d80852cba5a405a32cd6b
琅玡-琪拉 <1076963452@qq.com>
2024-10-09 21:35:11 +0800

    微信小程序分享功能禁用

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ import constConfig from './const.js'
 Vue.use(uView);
 
 // 引入uView对小程序分享的mixin封装
-let mpShare = require('uview-ui/libs/mixin/mpShare.js');
-Vue.mixin(mpShare);
+//let mpShare = require('uview-ui/libs/mixin/mpShare.js');
+//Vue.mixin(mpShare);
 // 判断是否登录
 let hasLogin = require('@/mixins/hasLogin.js');
 Vue.mixin(hasLogin);
