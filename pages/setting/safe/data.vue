c9e01cf7e39c9ec13c8001936cc0328b3427f803
琅玡-琪拉 <1076963452@qq.com>
2024-11-02 12:23:00 +0800

    导出账单安全升级

--- pages/setting/safe/data.vue
+++ pages/setting/safe/data.vue
@@ -4,18 +4,11 @@
       <view class="menu-list">
         <u-cell-group>
 
-          <u-cell-item @click="export_data()" title="数据导出" icon="download">
-
-            <u-link v-if="file_url"
-                    :href="file_url"
-                    @click="copy(file_url)"
-                    :under-line="false"
-                    :copy="true"
-            >点击复制到浏览器打开，5分钟有效</u-link>
-            <text v-else>导出已有账单数据</text>
+          <u-cell-item @click="goto('/pages/setting/safe/export_data')" title="导出账单" icon="download">
+            <text >导出已有账单数据</text>
           </u-cell-item>
-          <u-cell-item @click="goto('/pages/setting/safe/import_data')" :icon-style="{transform: 'rotate(180deg)'}" title="数据导入" icon="download">
-            导入其它数据
+          <u-cell-item @click="goto('/pages/setting/safe/import_data')" :icon-style="{transform: 'rotate(180deg)'}" title="导入账单" icon="download">
+            导入微信、支付宝等账单
           </u-cell-item>
 
         </u-cell-group>
@@ -37,79 +30,6 @@ export default {
   },
 
   methods: {
-    // 查看文件
-    readFile(e) {
-      uni.showLoading({
-        title:'请求中...'
-      })
-      // #ifdef APP-PLUS
-      uni.downloadFile({
-        url: e,
-        success: function(res) {
-          console.log(res)
-          var filePath = res.tempFilePath;
-          uni.openDocument({
-            filePath: filePath,
-            success: function(res) {
-              console.log('打开文档成功');
-              uni.hideLoading();
-            },
-            fail(err) {
-              console.log(err)
-              uni.hideLoading();
-              uni.showToast({
-                title: err,
-                icon: 'none'
-              });
-            }
-          });
-        },
-        fail(err) {
-          console.log(err)
-          uni.hideLoading();
-          uni.showToast({
-            title: err,
-            icon: 'none'
-          });
-        },
-        complete() {
-          uni.hideLoading();
-        }
-      });
-      // #endif
-      // #ifndef APP-PLUS
-      let that = this
-      setTimeout(function (){
-        that.file_url = e
-        uni.hideLoading()
-      },2000)
-
-      // #endif
-    },
-    copy(e) {
-      uni.setClipboardData({
-        data: e,
-        success: function() {
-          uni.showToast({
-            title: '复制成功'
-          })
-        }
-      })
-    },
-
-    export_data(){
-      if (this.file_url){
-        this.copy(this.file_url)
-        return this.$u.toast('已生成，请复制到浏览器打开')
-      }
-      this.$u.api.export_data().then(res => {
-        if (res.code !== 0) {
-          this.$u.toast(res.msg)
-        } else {
-          this.readFile(res.data.url)
-        }
-      })
-    },
     goto(path, auth = true) {
       if (auth && !this.hasLogin) {
         this.$u.toast('请登录后查看');
---
+++ pages/setting/safe/export_data.vue
@@ -0,0 +1,123 @@
+<template>
+  <view class="container bs_form">
+    <u-form-item label="接收邮箱" label-width="150">
+      <u-input placeholder="请输入邮箱" border :maxlength="60" type="email" clearable class="category_form_input"
+               v-model="form.email"/>
+    </u-form-item>
+    <u-form-item label="登录密码" v-if="form.email" label-width="150">
+      <u-input placeholder="请输入登录密码" border :maxlength="32" type="password" clearable class="category_form_input"
+               v-model="form.password"/>
+    </u-form-item>
+    <u-form-item label="操作说明" label-width="150">
+      <view class="u-text-left">
+        请确保邮箱地址正确，否则无法接收到账单。为了数据安全，导出的账单已设置解压密码，对应的密码请在消息中心中查看。
+      </view>
+
+    </u-form-item>
+
+    <view class="buttons">
+      <u-button class="action-btn" type="success" @click="submit">提交导出账单</u-button>
+
+    </view>
+  </view>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+    };
+  },
+  onLoad(options) {
+    this.getUserInfo()
+  },
+  mounted() {
+
+  },
+  methods: {
+    getUserInfo() {
+      let user = this.$store.getters.user
+      if (!user) {
+        //跳到登录
+        uni.navigateTo({
+          url: '/pages/login/login'
+        })
+        return;
+      }
+      this.$nextTick(() => {
+        this.form.email = user.email
+      })
+    },
+
+
+
+    submit() {
+      if (!this.form.email) {
+        this.$u.toast('请输入邮箱');
+        return;
+      } else if (!this.form.email.match(/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/)) {
+        this.$u.toast('请输入正确的邮箱');
+        return;
+      }
+      if (!this.form.password) {
+        this.$u.toast('请输入登录密码');
+        return;
+      }
+
+      uni.showModal({
+        title: '',
+        content: '确定提交导出账单吗？',
+        success: (res) => {
+          if (res.confirm) {
+            uni.showLoading({
+              title: '导出中...'
+            })
+            this.$u.api.export_data(this.form).then(res => {
+              this.$u.toast(res.msg)
+              if (res.code === 0) {
+                 setTimeout(function () {
+                  uni.navigateBack({
+                    delta: 1
+                  })
+                }, 3000)
+              }
+            }).finally(()=>{
+              uni.hideLoading()
+            })
+
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.bs_form {
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+
+  .input-code {
+    width: 80rpx;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 160rpx;
+
+    .action-btn {
+      width: 100%;
+      border-radius: unset !important;
+    }
+  }
+}
+
+</style>
